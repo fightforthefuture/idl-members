3172485f81aacd5aea981df7cea41d1a9222e824
Chuck Harmston <chuck@chuckharmston.com>
2012-07-27 14:02:19 -0400

    Refactors ReachView to use cached value, rather than template tag

--- internetdefense/apps/analytics/views.py
+++ internetdefense/apps/analytics/views.py
@@ -1,6 +1,9 @@
+from django.core.cache import cache
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
+
+from analytics.settings import CACHE_KEY_REACH
 
 
 class ReachView(TemplateView):
@@ -16,3 +19,12 @@ class ReachView(TemplateView):
         Caches the view for 24 hours.
         """
         return super(ReachView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds QuerySets of all active campaigns and presentational variants to
+        the template context.
+        """
+        context = super(ReachView, self).get_context_data(**kwargs)
+        context['reach'] = cache.get(CACHE_KEY_REACH)
+        return context
