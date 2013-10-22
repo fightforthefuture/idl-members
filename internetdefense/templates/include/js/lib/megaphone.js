d0c5d3feef962c9a1f27a2c09e28cc9f8e6fe7bb
Chris Antaki <ChrisAntaki@gmail.com>
2013-10-22 12:02:49 -0700

    Debug code was removed

--- internetdefense/templates/include/js/lib/megaphone.js
+++ internetdefense/templates/include/js/lib/megaphone.js
@@ -15,11 +15,6 @@
         return;
     }
 
-    window.tfrce_config = {
-        // debug: true,
-        disableGeo: true // If true, will show to all visitors not just those around Washington DC
-    };
-
     var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
     e.src = document.location.protocol + '//d1ux67szpr7bp0.cloudfront.net/project-megaphone/widget.min.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
