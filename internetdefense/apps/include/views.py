a38690567a9527754811cf8454312dfd3cbc3c91
Chris Antaki <ChrisAntaki@gmail.com>
2013-10-24 09:37:27 -0700

    Project Megaphone was activated

--- internetdefense/apps/include/views.py
+++ internetdefense/apps/include/views.py
@@ -202,7 +202,7 @@ class IncludeView(IncludeMixin, TemplateView):
         Otherwise, set context data and return the appropriate response.
         """
         
-        project_megaphone_is_active = False
+        project_megaphone_is_active = True
 
         if not project_megaphone_is_active and not self.get_campaign() and not self.is_test():
             return HttpResponse()
--- internetdefense/templates/include/js/include.js
+++ internetdefense/templates/include/js/include.js
@@ -4,13 +4,29 @@
     {% include 'include/js/lib/cookies.js' %}
     {% include 'include/js/lib/querystring.js' %}
 
-    // Ensure that user hasn't opted out
+    // Ensure that user hasn't opted out.
     var cookieName = '_idl_opt_out_' + '{{ campaign.slug }}',
         optedOut = cookie.read(cookieName) == 'true';
 
     if (optedOut) {
         return;
     }
+
+    // Verify campaign.
+    var qs = querystring.decode();
+    if (_idl.campaign && _idl.campaign !== 'stopwatchingusrally') {
+        return 'Bailing';
+    }
+
+    // Set variant.
+    var variant = 'default';
+    if (qs.variant === 'modal') {
+        variant = 'modal';
+    }
+
+    window.tfrce_config = {
+        show_style: variant
+    };
 
     {% include 'include/js/lib/megaphone.js' %}
 
