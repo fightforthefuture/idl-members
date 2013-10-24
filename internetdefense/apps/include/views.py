31537dcb1039b5fbe7a2a2e109fcae22038d7c4b
Chris Antaki <ChrisAntaki@gmail.com>
2013-10-24 09:53:42 -0700

    Testing a fix for campaign specified embed

--- internetdefense/apps/include/views.py
+++ internetdefense/apps/include/views.py
@@ -108,13 +108,14 @@ class IncludeMixin(object):
 
         Otherwise, return the latest active campaign, if there are any.
         """
+        # For Project Megaphone
+        return True
+        
         if self.campaign is not None:
             return self.campaign
 
         slug = self.request.GET.get('campaign', None)
 
-        # For Project Megaphone
-        return slug
         
         if slug == 'NSA':
             slug = 'nsa'
