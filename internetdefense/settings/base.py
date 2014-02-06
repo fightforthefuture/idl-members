f35d5999fc89a18a20bf75be4366b5e704389f92
Chris Antaki <ChrisAntaki@gmail.com>
2014-02-06 11:15:03 -0800

    Added modern browser check

--- internetdefense/settings/base.py
+++ internetdefense/settings/base.py
@@ -55,13 +55,13 @@ TEMPLATE_LOADERS = [
 ]
 MIDDLEWARE_CLASSES = [
     'django.middleware.gzip.GZipMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     # 'ip2geo.middleware.CityMiddleware',
 ]
 INSTALLED_APPS = [
--- internetdefense/templates/include/js/include.js
+++ internetdefense/templates/include/js/include.js
@@ -7,7 +7,7 @@
     {% include 'include/js/lib/addevent.js' %}
     {% include 'include/js/lib/classname.js' %}
 
-    // Default settings
+    /* Default settings */
     var qs = querystring.decode(),
         canPostMessage = !!window.postMessage,
         defaults = {
@@ -19,31 +19,37 @@
         },
         ieQuirks = (document.compatMode != 'CSS1Compat') && (navigator.appVersion.indexOf("MSIE") != -1);
 
-    // Overrides defaults with settings in window._idl
+    /* Overrides defaults with settings in window._idl */
     for(var key in _idl){
         if(_idl.hasOwnProperty(key)){
             defaults[key] = _idl[key];
         }
     }
 
-    // Ensure that user hasn't opted out
+    /* Ensure that user hasn't opted out */
     var cookieName = '_idl_opt_out_' + '{{ campaign.slug }}',
         opted_out = cookie.read(cookieName) == 'true';
-        
-        var ua = navigator.userAgent.toLowerCase();
-        var isAndroid = ua.indexOf("android") > -1;
-
-    // disable mobile and small screens
-    if(!(window.innerWidth <= 800 && window.innerHeight <= 800) && !(navigator.userAgent.match(/iPhone/i) || isAndroid)){
-        if (_idl.variant === 'banner' && (!_idl.campaign || _idl.campaign === 'day-we-fight-back')) {
-            // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt GPL-v3-or-Later
-            (function(){
-                var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
-                e.src = document.location.protocol + '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
-            })();
-            // @license-end
-        }
+    if (opted_out) {
+        return;
     }
+
+    /* Require modern browsers. */
+    if (!document.addEventListener) {
+        return;
+    }
+
+    /* Require all campaigns to be enabled. */
+    if (_idl.variant !== 'banner' || _idl.campaign) {
+        return;
+    }
+
+    /* The Day We Fight Back :: JavaScript */
+    /* @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt GPL-v3-or-Later */
+    (function(){
+        var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
+        e.src = document.location.protocol + '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
+    })();
+    /* @license-end */
 
     {% include 'include/js/lib/closure_finish.js' %}
